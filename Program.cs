f01e6dc11b896294a02ef2dfa9a0f1861dbebba4
Raphael FB <jvstraphael@gmail.com>
2023-03-27 23:37:30 +0200

    calculating vat

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿public class MyApp
+{ 
+    public static void Main ()
+    {
+       float vat = 15f;
+       float productA = 3.99f;
+       float productB = 11.99f;
+       float productC = 5.75f;
+       
+       float summed = productA + productB + productC;
+       float totalTax = (summed/100) * vat;
+       
+       summed += totalTax;
+       
+       Console.WriteLine("Total Price is: " + summed);
+    }
+}
